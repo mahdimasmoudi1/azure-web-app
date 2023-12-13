5f7232c47672b0a72c3a68d4a0bd89ae4e2f93dc
Masmoudi Mahdi <122725004+mahdimasmoudi1@users.noreply.github.com>
2023-12-13 04:38:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
-
+const path = require("path");
 const app = express();
 
 app.use(express.json());
@@ -40,6 +40,11 @@ app.post("/create", (req, res) => {
     .save()
     .then((user) => res.json(user))
     .catch((err) => console.log(err));
+});
+
+app.use(express.static("./client/build"));
+app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname, "client","build" , "index.html"));
 });
 
 app.listen(port, () => {
